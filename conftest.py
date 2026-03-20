d729dd1229554a40bdf21a0db070ea68abc70199
Dasha_Menshchikova <dashakuritsyna@gmail.com>
2026-03-20 22:08:48 +0500

    add asserts

--- conftest.py
+++ conftest.py
@@ -9,6 +9,9 @@ def create_user_fixt():
     data = generate_fake_data()
     requests.post(Endpoints.REGISTER, json=data)
     log_response = requests.post(Endpoints.LOGIN, json=data)
+    assert log_response.status_code == 200
+    assert log_response.json()["success"] is True
+    assert "accessToken" in log_response.json()
     access_token = log_response.json()["accessToken"]
     yield data, access_token
     headers = {"Authorization": access_token}
